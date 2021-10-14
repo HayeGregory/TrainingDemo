ac2b17bcd884d40bbb178e3e8aa3538ebba9c93c
HayeGregory <haye.greg@outlook.com>
2021-10-14 09:37:53 +0200

    Initial commit

---
+++ src/test/java/be/bstorm/gha/TrainingDemo/TrainingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package be.bstorm.gha.TrainingDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
